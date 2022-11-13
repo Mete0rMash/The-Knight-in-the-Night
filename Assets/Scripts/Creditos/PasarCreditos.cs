cacd34aa97ed0d9e96be41ed61e0e283a9fd47fd
HIRONDELL <martiraf@hotmail.com>
2022-11-13 18:31:29 -0300

    Update PasarCreditos.cs

--- Assets/Scripts/Creditos/PasarCreditos.cs
+++ Assets/Scripts/Creditos/PasarCreditos.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class PasarCreditos : MonoBehaviour
 {
     // Start is called before the first frame update
