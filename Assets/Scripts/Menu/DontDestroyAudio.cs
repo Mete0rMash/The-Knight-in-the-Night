8bf6f1e88d0d9365382faec609cdce65ed641ccc
HIRONDELL <martiraf@hotmail.com>
2022-11-20 20:16:16 -0300

    HUD y Script DontDestroyAudio
    -Se creo el HUD con su respectivo slider para la vida, Imágenes para los objetos del PJ y también se agrego texto para contabilizar el dinero obtenido.
    -Respecto al script se logro pausar el tema del menú al cambiar escena, solo falta que se vuelva a reproducir al regresar al mismo

--- Assets/Scripts/Menu/DontDestroyAudio.cs
+++ Assets/Scripts/Menu/DontDestroyAudio.cs
@@ -1,23 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyAudio : MonoBehaviour
 {
+    public static DontDestroyAudio instance;
     private void Awake()
     {
-        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("MenuMusic");
-        //GameObject[] JuegoMusic = GameObject.FindGameObjectsWithTag("JuegoMusic");  //esto y lo otro comentado funciona después tengo que ver de arreglarlo
-        if (musicObj.Length > 1)
+        if(instance != null)
         {
-            Destroy(this.gameObject);
+            Destroy(gameObject);
         }
-        DontDestroyOnLoad(this.gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        } //Me reproduce el tema pero al volver al menú se corta... sirve pero tengo que ver como lo arreglo.
 
-        //if (JuegoMusic.Length > 1)
-       //{
-            //Destroy(this.gameObject);
-        //}
+
+            //Con esta linea de codigo el audio se sigue reproduciendo, pero lastimosamente no se destruye en Escena 1
+            /*GameObject[] musicObj = GameObject.FindGameObjectsWithTag("MenuMusic");
+            if (musicObj.Length > 1)
+            {
+                Destroy(this.gameObject);
+            }
+            
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }*/
+            
+
         
+
+    }
+
+    //Para Pausar el tema pero no vuelve a reproducirse cuando se vuelve al menú
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Escenario_1")
+        {
+            DontDestroyAudio.instance.GetComponent<AudioSource>().Pause();
+        }
+
+        //Vuelve a reproducir el tema "Ver bien porq no funca... tal vez lo borre."
+        /*if (SceneManager.GetActiveScene().name == "Menu_Principal")
+        {
+            //DontDestroyAudio.instance.GetComponent<AudioSource>().Play();
+            //DontDestroyOnLoad(this.gameObject);
+        }   */    
     }
 }
+
