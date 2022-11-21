890ed9e22f5e93fc5b0945b2755eedb5952faccd
HIRONDELL <martiraf@hotmail.com>
2022-11-20 21:06:55 -0300

    Comentarios y código extra eliminados

--- Assets/Scripts/Menu/DontDestroyAudio.cs
+++ Assets/Scripts/Menu/DontDestroyAudio.cs
@@ -16,27 +16,10 @@ public class DontDestroyAudio : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(this.gameObject);
-        } //Me reproduce el tema pero al volver al menú se corta... sirve pero tengo que ver como lo arreglo.
-
-
-            //Con esta linea de codigo el audio se sigue reproduciendo, pero lastimosamente no se destruye en Escena 1
-            /*GameObject[] musicObj = GameObject.FindGameObjectsWithTag("MenuMusic");
-            if (musicObj.Length > 1)
-            {
-                Destroy(this.gameObject);
-            }
-            
-        else
-        {
-            DontDestroyOnLoad(this.gameObject);
-        }*/
-            
-
-        
-
+        } //Me reproduce el tema pero al volver al menú se corta... Arreglado.
     }
 
-    //Para Pausar el tema pero no vuelve a reproducirse cuando se vuelve al menú
+    //Para Pausar el tema al ir a jugar.
     private void Update()
     {
         if (SceneManager.GetActiveScene().name == "Escenario_1")
@@ -46,22 +29,15 @@ public class DontDestroyAudio : MonoBehaviour
 
         //Aca abajo esta el arreglo que hice
 
-        /*
+        
         else if(SceneManager.GetActiveScene().name == "Menu_Principal")   // me fijo que sea el menu
         {            
             if (!DontDestroyAudio.instance.GetComponent<AudioSource>().isPlaying)   // me fijo que no se este reproduciendo
             {                
-                DontDestroyAudio.instance.GetComponent<AudioSource>().Play(); // reprodusco
+                DontDestroyAudio.instance.GetComponent<AudioSource>().Play(); // reproduzco
             }
         }
-        */
 
-        //Vuelve a reproducir el tema "Ver bien porq no funca... tal vez lo borre."
-        /*if (SceneManager.GetActiveScene().name == "Menu_Principal")
-        {
-            //DontDestroyAudio.instance.GetComponent<AudioSource>().Play();
-            //DontDestroyOnLoad(this.gameObject);
-        }   */    
     }
 }
 
