4911263e9632cb23a1640b3dcd91427162fbedfa
HIRONDELL <martiraf@hotmail.com>
2022-11-26 01:54:44 -0300

    Scripts corregidos

--- Assets/Scripts/Menu/DontDestroyAudio.cs
+++ Assets/Scripts/Menu/DontDestroyAudio.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,18 +8,18 @@ public class DontDestroyAudio : MonoBehaviour
     public static DontDestroyAudio instance;
     private void Awake()
     {
-        if(instance != null)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(this.gameObject);
-        } //Me reproduce el tema pero al volver al menú se corta... Arreglado.
+        
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            } //Me reproduce el tema pero al volver al menï¿½ se corta... Arreglado.
+        
     }
-
-    //Para Pausar el tema al ir a jugar.
     private void Update()
     {
         if (SceneManager.GetActiveScene().name == "Escenario_1")
@@ -29,15 +29,15 @@ public class DontDestroyAudio : MonoBehaviour
 
         //Aca abajo esta el arreglo que hice
 
-        
-        else if(SceneManager.GetActiveScene().name == "Menu_Principal")   // me fijo que sea el menu
-        {            
+
+        else if (SceneManager.GetActiveScene().name == "Menu_Principal")   // me fijo que sea el menu
+        {
             if (!DontDestroyAudio.instance.GetComponent<AudioSource>().isPlaying)   // me fijo que no se este reproduciendo
-            {                
+            {
                 DontDestroyAudio.instance.GetComponent<AudioSource>().Play(); // reproduzco
             }
         }
 
     }
-}
 
+}
--- Assets/Scripts/Menu/DontDestroyGameSound.cs
+++ Assets/Scripts/Menu/DontDestroyGameSound.cs
@@ -8,7 +8,7 @@ public class DontDestroyGameSound : MonoBehaviour
     public static DontDestroyGameSound instance;
     private void Awake()
     {
-        if(instance != null)
+        if (instance != null)
         {
             Destroy(gameObject);
         }
@@ -19,22 +19,22 @@ public class DontDestroyGameSound : MonoBehaviour
         } //Me reproduce el tema pero al volver al menú se corta... Arreglado.
     }
 
-    //Para Pausar el tema al ir al menu.
+    //Para Pausar el tema al ir a jugar.
     private void Update()
     {
         if (SceneManager.GetActiveScene().name == "Menu_Principal")
         {
-            DontDestroyAudio.instance.GetComponent<AudioSource>().Pause();
+            DontDestroyGameSound.instance.GetComponent<AudioSource>().Pause();
         }
 
         //Aca abajo esta el arreglo que hice
 
-        
-        else if(SceneManager.GetActiveScene().name == "Escenario_1")   // me fijo que sea el escenario 1
-        {            
-            if (!DontDestroyAudio.instance.GetComponent<AudioSource>().isPlaying)   // me fijo que no se este reproduciendo
-            {                
-                DontDestroyAudio.instance.GetComponent<AudioSource>().Play(); // reproduzco
+
+        else if (SceneManager.GetActiveScene().name == "Escenario_1")   // me fijo que sea el menu
+        {
+            if (!DontDestroyGameSound.instance.GetComponent<AudioSource>().isPlaying)   // me fijo que no se este reproduciendo
+            {
+                DontDestroyGameSound.instance.GetComponent<AudioSource>().Play(); // reproduzco
             }
         }
 
