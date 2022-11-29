2d9f230987ebf9042407ab845974f6e584e2fcd7
Mete0rMash <ariel.a.sfeir@gmail.com>
2022-11-29 02:38:27 -0300

    Player Attack

---
+++ Assets/Scripts/Intermediaries/WeaponAnimationToWeapon.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAnimationToWeapon : MonoBehaviour
+{
+    private Weapon weapon;
+
+    private void Start()
+    {
+        weapon = GetComponentInParent<Weapon>();
+    }
+
+    private void AnimationFinishTrigger()
+    {
+        weapon.AnimationFinishTrigger();
+    }
+
+}
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator baseAnimator;
+    private Animator weaponAnimator;
+
+    protected PlayerAttackState state;
+
+    protected virtual void Start()
     {
-        
+        baseAnimator = transform.Find("Base").GetComponent<Animator>();
+        weaponAnimator = transform.Find("Weapon").GetComponent<Animator>();
+
+        gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void EnterWeapon()
     {
-        
+        gameObject.SetActive(true);
+
+        baseAnimator.SetBool("attack", true);
+        weaponAnimator.SetBool("attack", true);
+    }
+
+    public virtual void ExitWeapon()
+    {
+        gameObject.SetActive(false);
+
+        baseAnimator.SetBool("attack", false);
+        weaponAnimator.SetBool("attack", false);
+    }
+
+    #region Animation Triggers
+
+    public virtual void AnimationFinishTrigger()
+    {
+        state.AnimationFinishTrigger();
+    }
+
+    #endregion
+
+    public void InitializeWeapon(PlayerAttackState state)
+    {
+        this.state = state;
     }
 }
