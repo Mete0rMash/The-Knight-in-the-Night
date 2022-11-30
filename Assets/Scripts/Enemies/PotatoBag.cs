fcc8e6b84961b00f92abf32ca69f22ea44a4d2b9
verjil33 <91489461+verjil33@users.noreply.github.com>
2022-11-30 17:19:20 -0300

    PotatoBag

--- Assets/Scripts/Enemies/PotatoBag.cs
+++ Assets/Scripts/Enemies/PotatoBag.cs
@@ -1,17 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class PotatoBag : MonoBehaviour
 {
 
-    [SerializeField] private Text damageIndicator;
+    [SerializeField] private TextMeshPro damageIndicator;
 
     [SerializeField] private GameObject gameText;
-     
+
+    [SerializeField] private float time;
+
+    [SerializeField] private int _damage;
+
+    bool runingTime = false;
+
+
+    private bool runTime()
+    {
+        time += Time.deltaTime;
+        if(time > 3)
+        {
+            runingTime = false;
+            time = 0;
+            return false;
+            
+        }
+        runingTime = true;
+        return true;        
+    }
+
+
+    private void Update()
+    {
+        /*
+        if (runingTime)
+        {
+            if (runTime())
+            {
+                damageIndicator.text = _damage.ToString();
+            }
+            else
+            {
+                damageIndicator.text = "";
+            }
+        }
+        */
+        if (runTime())
+        {
+            damageIndicator.text = _damage.ToString();
+        }
+        else
+        {
+            damageIndicator.text = "";
+        }
+
+    }
+
+
     public void GetDamage(int damage)
-    {       
-        gameText.GetComponent<TextMesh>().text = damage.ToString();
+    {        
+        damageIndicator.text = damage.ToString();
+        _damage = damage;
+        runingTime = true;
     }
 }
